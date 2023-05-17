d57dee03065dc056a455d867e6cc3ed3a05ebd43
Vishalkumar Rana <98719268+VRANA13@users.noreply.github.com>
2023-05-17 10:45:35 -0400

    Update server.js

--- server.js
+++ server.js
@@ -7,9 +7,9 @@ app.get("/", (req, res) => {
     var i;
 i = 10;
     while(i === 10){
-    res.send("Hello World!");
-    res.send("Vishalkumar Rana - 151059219");
-    res.send("My favorite movie is The Lion King");
+   // res.send("Hello World!");
+    res.end("Vishalkumar Rana - 151059219");
+    res.write("My favorite movie is The Lion King");
     }
 });
 
